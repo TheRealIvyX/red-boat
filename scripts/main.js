a96c527eca403d854582046bd9b66c2d0bf93022
IvyX <24882599+TheRealIvyX@users.noreply.github.com>
2021-05-01 14:26:55 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,11 @@ const yes = extendContent(UnitType, "red-boat", {});
 yes.constructor = () => extend(UnitWaterMove, {});
 
 Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(yes, 1, ItemStack.with(Items.thorium, 1)));
+
+
+const a = extendContent(UnitType, "the-dad", {});
+a.constructor = () => extend(UnitWaterMove, {});
+a.immunities.add(StatusEffects.corroded);
+
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "red-boat-red-boat"), Vars.content.getByName(ContentType.unit, "red-boat-the-dad")]);
+Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
